46fd5295a3730b2ae5ded22f2405c00ab0ff66f9
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-06-17 18:18:10 +0100

    Add GA layout algorithm

--- layout/main.py
+++ layout/main.py
@@ -2,10 +2,13 @@
 
 from random    import seed as set_seed
 from random    import sample
+from random    import choice
+from random    import randrange
 from itertools import product
 from itertools import combinations
 from functools import partial
 from operator  import itemgetter
+from copy      import deepcopy
 
 """
 Notes on data representation:
@@ -81,6 +84,22 @@ def get_rand_id_cons(layers, connections, seed=1):
         id_cons[index] = sample(all_cons, con_count)
     return id_cons
 
+def get_rand_circuit_id_cons(layers, connections, seed=1):
+    # Generate random connectivity constrained to two incoming connections per
+    # gate (i.e. assuming two-input gates)
+    n = len(layers)
+    id_cons = [None] * (n-1)
+    set_seed(seed)
+    for ind in range(1, n):
+        gates = layers[ind]
+        prev_gates = range(layers[ind-1])
+        layer_cons = []
+        for gate in range(gates):
+            for src in sample(prev_gates, 2):
+                layer_cons.append((src, gate))
+        id_cons[ind-1] = layer_cons
+    return id_cons
+
 def get_po_cons(id_cons, arrangement):
     # Generated positional (i.e. ordered) connectivity for id_cons given an
     # arrangement
@@ -116,7 +135,42 @@ def arrange_busy_top(layers, id_cons, toggle_reverse = True):
         arrangement.append(sorted_inds)
     return arrangement
 
-def arrange_best_of_n(layers, id_cons, n=5000):
+def arrange_ga(layers, id_cons):
+
+    npop  = 20  # population size
+    srate = 0.1 # selection rate (fraction of top individuals to reproduce)
+    iters = 15  # number of iterations
+
+    scount = int(srate * npop) # selection count
+
+    nlayers = len(layers)
+    layer_lists = [range(x) for x in layers]
+
+    def mutated(arrangement):
+        # Return mutated solution
+        new_arr = deepcopy(arrangement)
+        mlayer = randrange(nlayers)
+        gates = layer_lists[mlayer]
+        a, b = sample(gates, 2)
+        new_arr[mlayer][a], new_arr[mlayer][b] = \
+            new_arr[mlayer][b], new_arr[mlayer][a]
+        return new_arr
+
+    def eval_(arrangement):
+        # return solution cost
+        po_cons = get_po_cons(id_cons, arrangement)
+        return count_cross(po_cons)
+
+    pop = [arrange_rand(layers, id_cons) for _ in range(npop)]
+
+    for i in range(iters):
+        repr_inds = [randrange(scount) for _ in range(npop)]
+        children = [mutated(pop[ind]) for ind in repr_inds]
+        pop = sorted(children, key=eval_)
+
+    return pop[0]
+
+def arrange_best_of_n(layers, id_cons, n=300):
     get_po_cons_p = partial(get_po_cons, id_cons)
     arrs = [arrange_rand(layers, id_cons) for _ in range(n)] # random arrs
     po_cons = map(get_po_cons_p, arrs) # corresponding position connections
@@ -129,7 +183,7 @@ def get_distrib(layers, connections, arrange_fun, nsamples=1000):
     # Generate list of crossed connection counts, sampled from
     # randomly-generated id_cons
     def get_sample(seed):
-        id_cons = get_rand_id_cons(layers, connections, seed)
+        id_cons = get_rand_circuit_id_cons(layers, connections, seed)
         arrangement = arrange_fun(layers, id_cons)
         po_cons = get_po_cons(id_cons, arrangement)
         return count_cross(po_cons)
@@ -138,7 +192,7 @@ def get_distrib(layers, connections, arrange_fun, nsamples=1000):
 def print_test(layers, connections):
     # Perform basic function tests and print results
     arrangement = [range(x) for x in layers]
-    id_cons = get_rand_id_cons(layers, connections)
+    id_cons = get_rand_circuit_id_cons(layers, connections)
     po_cons = get_po_cons(id_cons, arrangement)
     print_cons(id_cons, "id_cons")
     print_cons(po_cons, "po_cons")
@@ -158,14 +212,18 @@ def eval_arr_algo(layers, connections, arr_algo, nsamples):
 
 def main():
 
-    layers = [5, 5, 5] # gates in each layer
-    connections = [10, 10] # connections between adjacent layers
+    depth = 2 # gates per layer
+    layers = [5] * depth # layer list
+    connections = [] # connections between adjacent layers
 
     # id_cons stores connections between gate ids
     # po_cons stores connections between gate positions
 
     # print_test(layers, connections)
-    eval_arr_algo(layers, connections, arrange_best_of_n, 1)
+    eval_arr_algo(layers, connections, arrange_ga, 100)
+
+    # id_cons = get_rand_circuit_id_cons(layers, connections)
+    # arrange_ga(layers, id_cons)
 
 if __name__ == "__main__":
     main()
