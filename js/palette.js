116c7167fe1d8927cf5dd9919f95c79d67c7694f
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-08-02 15:25:48 +0100

    Extract inline palette styles

--- js/palette.js
+++ js/palette.js
@@ -174,7 +174,7 @@ function paletteController($scope, $sce) {
 }
 
 module.directive('palette', () => ({
-    templateUrl: 'htm/comp_palette.htm',
+    templateUrl: 'htm/palette.htm',
     controller: paletteController,
 }));
 
