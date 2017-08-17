7e000268708a72d3b1a31b1284fc9f80e6537c3e
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-08-17 15:56:21 +0100

    Add hide-label class to poets.css

--- py/poets.py
+++ py/poets.py
@@ -12,7 +12,7 @@ corePoets = {
     "image": "poets/poets_logo_white.svg",
     "description": "System Core",
     "class": "poets",
-    "classes": ["animate"],
+    "classes": ["animate", "hide-label"],
     "width": 100,
     "height": 100,
     "x": 200,
