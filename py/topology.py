7f3a1d2a2414d9a51a889bc6a6083ae22c596d4c
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-08-18 22:18:12 +0100

    Add docstrings to topology.py

--- py/topology.py
+++ py/topology.py
@@ -3,17 +3,19 @@ import copy
 
 
 def drange(w, h):
+    """Return cartesian product of [1..w] and [1..h]"""
     return itertools.product(range(w), range(h))
 
 
-def extend(dict_, attrs):
-    extended = copy.deepcopy(dict_)
-    for key, val in attrs.items():
-        extended[key] = val
-    return extended
+def merge(dict1, dict2):
+    """Return copy of dict_ with added items from attrs"""
+    merged = copy.deepcopy(dict1)
+    merged.update(dict2)
+    return merged
 
 
 def get_grid(module_template, w, h, module_name="module_%d_%d"):
+    """Return tuple (modules, connections) representin an wxh module grid"""
 
     r_port = "E"
     l_port = "W"
@@ -30,18 +32,21 @@ def get_grid(module_template, w, h, module_name="module_%d_%d"):
         return (200 * i, -200 * j)
 
     for i, j in drange(w, h):
+        """Instantiate modules"""
         mod_id = get_id(i, j)
         x, y = get_coords(i, j)
         attrs = {"id": mod_id, "x": x, "y": y}
-        modules[mod_id] = extend(module_template, attrs)
+        modules[mod_id] = merge(module_template, attrs)
 
     for i, j in drange(w - 1, h):
+        """Add horizontal connections"""
         core1, core2 = get_id(i, j), get_id(i + 1, j)
         modules[core1]["ports"][r_port] = {"position": "right"}
         modules[core2]["ports"][l_port] = {"position": "left"}
         connections.append([core1, core2, r_port, l_port])
 
     for i, j in drange(w, h - 1):
+        """Add vertical connections"""
         core1, core2 = get_id(i, j), get_id(i, j + 1)
         modules[core1]["ports"][t_port] = {"position": "top"}
         modules[core2]["ports"][b_port] = {"position": "bottom"}
