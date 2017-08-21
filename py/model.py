4e0effcbb099f6e829b6bcc128f3866c316d05b2
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-08-21 23:22:45 +0100

    Add stub model.py

---
+++ py/model.py
@@ -0,0 +1,16 @@
+class Model:
+
+    modules = []
+    connections = []
+
+    def add_module(id_, attrs):
+        pass
+
+    def remove_module(id_):
+        pass
+
+    def add_connection(mod1, mod2, port1, port2):
+        pass
+
+    def reset():
+        pass
